fa7ecea9f6b3d2f77c346c5070f075b16669512c
Debmalya Biswas <viperbale.db@gmail.com>
2023-02-16 20:11:55 +0530

    pre-final changes 3.6

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,23 +7,47 @@ import Timeline from './Timeline'
 import Loader from './Loader'
 import FAQs from './FAQs'
 import Thistime from'./Thistime'
+// import Timer from './Timer'
 const LandingPage = () => {
-  useEffect(() => {
+//   const [days, setDays] = useState(0);
+//     const [hours, setHours] = useState(0);
+// const [minutes, setMinutes] = useState(0);
+// const [seconds, setSeconds] = useState(0);
+
+// const deadline = "February 17, 2023 11:00:00"
+// // const deadline = "February 16, 2023 11:56:00"
+
+// const  getTime = () =>{
+//     const timeleft = Date.parse(deadline)- Date.now()
+//     setDays(Math.floor(timeleft / (1000 * 60 * 60 * 24)))
+//     setHours(Math.floor(
+//         (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//       ))
+//     setMinutes(Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60)))
+//     setSeconds(Math.floor((timeleft % (1000 * 60)) / 1000))
+//     console.log( timeleft / (1000 * 60 * 60 * 24));
+// }
+// useEffect(() => {
+// setInterval(getTime, 1000)
+// return () => clearInterval()   
+// }, [ ])
+useEffect(() => {
 		window.history.scrollRestoration = "manual";
 	}, []);
-  useEffect(() => {
+useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
   const [loading, setLoading] = useState(false)
-  useEffect(()=>{
+useEffect(()=>{
     setLoading(true)
     setTimeout(()=>{
       setLoading(false)
     },3000)
-  },[])
+  },[ ])
   return (
     <div>
         {
+          // (days+hours+minutes+seconds<=0)?
           loading?(
             <Loader></Loader>
           ):
@@ -38,6 +62,12 @@ const LandingPage = () => {
                   <FAQs/>
             </div>
           )
+          // :
+          // <Timer
+          // days={days} hours={hours} minutes={minutes} seconds={seconds}
+          // >
+
+          // </Timer>
         }
         
     </div>
---
+++ src/components/Timer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../styles/timer.scss'
+import logo from '../assets/logo1.png'
+const Timer = ({days, hours, minutes,seconds}) => {
+  return (
+    <div className="timeline_container">
+      <div className="logo_img">
+        <img src={logo} alt="" />
+      </div>
+      <div className='timer_wrapper'>
+      <div className='timeline_content'>
+            <h1>{days<10?"0"+days:days}</h1>
+            <p>Days</p>
+            </div>
+        <div  className='timeline_content'>
+            <h1>{hours<10?"0"+hours:hours}</h1>
+            <p>Hours</p>
+            </div>
+        <div  className='timeline_content'>
+            <h1>{minutes<10?"0"+minutes:minutes}</h1>
+            <p>Minutes</p>
+            </div>
+        <div  className='timeline_content'>
+            <h1>{seconds<10?"0"+seconds:seconds}</h1>
+            <p>Seconds</p>
+            </div>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Timer
